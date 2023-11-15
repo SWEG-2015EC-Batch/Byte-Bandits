3fa440386b5411b8c2a70a20255dd7aabd353e27
Zerubbabelmessele <149153936+Zerubbabelmessele@users.noreply.github.com>
2023-11-14 22:48:03 -0800

    Create ch-2 PE q4.cpp

---
+++ ETS1512_15_ZERUBBABEL_MESSELE/ch-2 PE q4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x, y;
+    cout << "Enter the value of X: ";
+    cin >> x;
+    cout << "Enter the value of Y: ";
+    cin >> y;
+    cout << "The result of the expression is " << pow(x, y) << endl;
+    return 0;
+}
