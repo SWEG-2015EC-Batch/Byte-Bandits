517b7aa51bcb6c7d3b808e77be3d82a5bc03c028
Robel Desalegn <130729370+Robeill@users.noreply.github.com>
2023-11-18 04:40:29 +0200

    Update Mileage.cpp

--- ETS1157_15_Robel_Desalegn/Assignment_2/Q2/Mileage.cpp
+++ ETS1157_15_Robel_Desalegn/Assignment_2/Q2/Mileage.cpp
@@ -1,11 +1,11 @@
-/*This program calculates the maximum miles an automobile can be driven
-based on the capacity of it's fuel tank and mile per gallon */
+/*This program calculates the maximum number of miles an automobile can be driven
+based on the capacity of its fuel tank and mile per gallon */
 #include <iostream>
 using namespace std;
 
 int main() 
 {
-  double tank_capacity{}, mpg{}, max_miles{};
+  double tank_capacity, mpg, max_miles;
   
   cout << "Enter the capacity of the fuel tank in gallons: ";
   cin >> tank_capacity;
