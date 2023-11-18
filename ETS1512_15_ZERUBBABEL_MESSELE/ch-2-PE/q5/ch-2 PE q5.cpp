915c3d60e405295fb7d9847186039880b17f5be0
Zerubbabelmessele <149153936+Zerubbabelmessele@users.noreply.github.com>
2023-11-18 00:43:39 -0800

    Update ch-2 PE q5.cpp

--- ETS1512_15_ZERUBBABEL_MESSELE/ch-2-PE/q5/ch-2 PE q5.cpp
+++ ETS1512_15_ZERUBBABEL_MESSELE/ch-2-PE/q5/ch-2 PE q5.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string name;
-    double weeklyWorkingHours, bonusRatePerHour, baseSalary, grossSalary, pension, tax, netSalary, bonusPayment;
-    cout << "Enter employee name: ";
-    getline(cin, name);
-    cout << "Enter weekly working hours: ";
-    cin >> weeklyWorkingHours;
-    cout << "Enter bonus rate per hour: ";
-    cin >> bonusRatePerHour;
-    cout << "Enter base salary: ";
-    cin >> baseSalary;
-    grossSalary = baseSalary + (weeklyWorkingHours * bonusRatePerHour);
-    pension = 0.05 * grossSalary;
-    tax = 0.15 * grossSalary;
-    netSalary = grossSalary - pension - tax;
-    bonusPayment = grossSalary - netSalary;
-    cout << "Employee name: " << name << endl;
-    cout << "Gross salary: $" << grossSalary << endl;
-    cout << "Pension: $" << pension << endl;
-    cout << "Tax: $" << tax << endl;
-    cout << "Net salary: $" << netSalary << endl;
-    cout << "Bonus payment: $" << bonusPayment << endl;
-    return 0;
+// Program to calculate gross salary, net salary and bonus of a employee
+// By: paulusel
+#include <iostream>
+#include <string>
+
+int main(){
+
+	const double tax = 0.15, pension = 0.05;
+
+	std::string name = "";
+	int work_hour;
+	double bonus_rate, base_salary;
+	double gross, net, bonus = 0;
+
+	std::cout << "Employee name, Please?" << std::endl;
+	std::cin >> name;
+
+	std::cout << "Base salary?" << std::endl;
+	std::cin >> base_salary;
+		
+	std::cout << "Total work hours?" << std::endl;
+	std::cin >> work_hour;
+
+	std::cout << "How much per hour for overtime?" << std::endl;
+	std::cin >> bonus_rate;
+
+	//input check
+	if( name == "" || work_hour < 0 || base_salary < 0 || bonus_rate < 0){
+		std::cout << "Invalid input! The name must not empty and all the values have to be non-negative."
+			<< std::endl;
+
+		return 1;
+	}
+
+	if( work_hour > 40 ){
+		bonus = (work_hour - 40)*bonus_rate;
+	}
+
+	gross = base_salary + bonus;
+	net = gross - (tax * gross) - ( gross * pension );
+
+	std::cout << "Employee Name: " << name << std::endl
+		<< "Gross Salary: " << gross << std::endl
+		<< "Bonus Payment: " << bonus << std::endl
+		<< "Net Salary: " << net << std::endl;
+
+	return 0;
 }
