c6ab891fbd41be38e3afdae31bd7598a6fef599c
Zerubbabelmessele <149153936+Zerubbabelmessele@users.noreply.github.com>
2023-11-18 00:55:43 -0800

    Update ch-2 PE q4.cpp

--- ETS1512_15_ZERUBBABEL_MESSELE/ch-2-PE/q4/ch-2 PE q4.cpp
+++ ETS1512_15_ZERUBBABEL_MESSELE/ch-2-PE/q4/ch-2 PE q4.cpp
@@ -1,30 +1,10 @@
-// Reads in a character, and prints its it out with its character inverted
-// By: paulusel
-#include <iostream>
-#include <cctype>
-
-int main(){
-
-	char letter = '\0', n_letter = '\0';
-
-	std::cout << "Enter a character and its case will be inverted." << std::endl;
-	std::cin >> letter;
-	
-	// not all characters are letters
-	if( !isalpha(letter) ){
-		std::cout << "That's not a letter!" << std::endl;
-
-		return 1;
-	}
-
-	if( islower(letter) ){
-		n_letter = toupper(letter);
-	}
-	else{
-		n_letter = tolower(letter);
-	}
-
-	std::cout << "The flipped letter is: " << n_letter << std::endl;
-
-	return 0;
+int main() {
+    double x, y;
+    cout << "Enter the value of X: ";
+    cin >> x;
+    cout << "Enter the value of Y: ";
+    cin >> y;
+    cout << "The result of the expression is " << x << endl;
+    return 0;
 }
+
