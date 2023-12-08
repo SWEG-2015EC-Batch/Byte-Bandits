d8e5aed36f56c6d770b03c176baa5d8e148dfb61
paulusel <paulusel@proton.me>
2023-12-08 23:22:45 +0000

    Add income-calculator code

---
+++ ETS0877_11_Paulos_Elias/Activity-3.1/Income-Calculator/income.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    const float PENSION_RATE = 0.07; // pension rate is 7%
+    float taxrate, ov_rate; // tax and pension rates
+    float gross, overtime = 0, net;
+    int w_hours; //work hours
+    
+    std::cout << "Input" << std::endl << "=====" << std::endl;
+    std::cout << "Gross Salary: ";
+    std::cin >> gross;
+    std::cout << "Work Hours: ";
+    std::cin >> w_hours;
+    std::cout << "Tax Rate(%): ";
+    std::cin >> taxrate;
+    std::cout << "Overtime Rate: ";
+    std::cin >> ov_rate;
+
+    if(w_hours > 40){
+        overtime = (w_hours - 40) * ov_rate;
+    }
+
+    taxrate /= 100;
+    net = gross + overtime - (gross * PENSION_RATE) - (gross * taxrate);
+
+    std::cout << std::endl << "Output" << std:: endl << "======" << std::endl;
+    std::cout << "Net salary of the employee is " << net << std::endl;
+
+    return 0;
+}
