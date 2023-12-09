07f79051de29204a415317c6e9563f2ee0a8972c
paulusel <paulusel@proton.me>
2023-12-09 00:15:50 +0000

    Add grade-calculator break-down version code

---
+++ ETS0877_11_Paulos_Elias/Activity-3.1/Grade-Calculator/grade-add.cpp
@@ -0,0 +1,54 @@
+// Converts numerical marks to letter grade
+// By: paulusel
+#include <iostream>
+
+int main(){
+    float marks;
+    float test, quiz, project, assign, final;
+
+    //std::cout << "Enter marks out of 100" << std::endl;
+    //std::cin >> marks;
+    std::cout << "Input" << std::endl << "=====" << std::endl;
+    std::cout << "Test(15%): ";
+    std::cin >> test;
+    std::cout << "Quiz(5%): ";
+    std::cin >> quiz;
+    std::cout << "Project(20%): ";
+    std::cin  >> project;
+    std::cout << "Assignment(10%): ";
+    std::cin >> assign;
+    std::cout << "Final(50%): ";
+    std::cin >> final;
+
+    marks = test + quiz + project + assign + final;
+
+    if(marks < 0 || marks > 100){
+        std::cout << "Not a valid mark!" << std::endl;
+    }
+    else if(marks < 30){
+        std::cout << "Grade: F" << std::endl;
+    }
+    else if(marks < 45){
+        std::cout << "Grade: D" << std::endl;
+    }
+    else if(marks < 55){
+        std::cout << "Grade: C" << std::endl;
+    }
+    else if(marks < 60){
+        std::cout << "Grade: C+" << std::endl;
+    }
+    else if(marks < 75){
+        std::cout << "Grade: B" << std::endl;
+    }
+    else if(marks < 80){
+        std::cout << "Grade: B+" << std::endl;
+    }
+    else if(marks < 90){
+        std::cout << "Grade: A" << std::endl;
+    }
+    else if(marks <= 100){
+        std::cout << "Grade: A+" << std::endl;
+    }
+
+    return 0;
+}
