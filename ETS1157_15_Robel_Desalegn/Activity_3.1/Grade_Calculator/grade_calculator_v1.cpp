6c70430c6596241fe28ea91bedd25e7a01cfe96b
Robel Desalegn <robeldesalegn07@gmail.com>
2023-12-10 04:38:26 +0300

    Add grade calculator

---
+++ ETS1157_15_Robel_Desalegn/Activity_3.1/Grade_Calculator/grade_calculator_v1.cpp
@@ -0,0 +1,49 @@
+/*This program converts a mark of a course entered from the keyboard to its corresponding letter-grade*/
+
+#include <iostream>
+
+int main()
+{
+    double mark;
+
+    std::cout << "Enter your mark: ";
+    std::cin >> mark;
+
+    if ((mark < 0) || (mark > 100))
+    {
+        std::cout << "\nInvalid mark";
+    }
+    else if (mark < 30)
+    {
+        std::cout << "\nYour grade is F";
+    }
+    else if (mark < 45)
+    {
+        std::cout << "\nYour grade is D";
+    }
+    else if (mark < 55)
+    {
+        std::cout << "\nYour grade is C";
+    }
+    else if (mark < 60)
+    {
+        std::cout << "\nYour grade is C+";
+    }
+    else if (mark < 75)
+    {
+        std::cout << "\nYour grade is B";
+    }
+    else if (mark < 80)
+    {
+        std::cout << "\nYour grade is B+";
+    }
+    else if (mark < 90)
+    {
+        std::cout << "\nYour grade is A";
+    }
+    else if (mark <= 100)
+    {
+        std::cout << "\nYour grade is A+";
+    }
+    return 0;
+}
---
+++ ETS1157_15_Robel_Desalegn/Activity_3.1/Grade_Calculator/grade_calculator_v2.cpp
@@ -0,0 +1,60 @@
+/*This program calculates the total mark of a course entered from the keyboard
+and conver it to its corresponding letter-grade */
+#include <iostream>
+
+int main()
+{
+    double mark;
+    double test, quiz, project, assignment, finalExam;
+
+    std::cout << "Enter your test mark(15%): ";
+    std::cin >> test;
+    std::cout << "Enter your quiz mark(5%): ";
+    std::cin >> quiz;
+    std::cout << "Enter your prokect mark(20%): ";
+    std::cin >> project;
+    std::cout << "Enter your assignment mark(10%): ";
+    std::cin >> assignment;
+    std::cout << "Enter your final exam mark(50%): ";
+    std::cin >> finalExam;
+
+    mark = test + quiz + project + assignment + finalExam;
+
+    if ((mark < 0) || (mark > 100))
+    {
+        std::cout << "\nInvalid mark";
+    }
+    else if (mark < 30)
+    {
+        std::cout << "\nYour grade is F";
+    }
+    else if (mark < 45)
+    {
+        std::cout << "\nYour grade is D";
+    }
+    else if (mark < 55)
+    {
+        std::cout << "\nYour grade is C";
+    }
+    else if (mark < 60)
+    {
+        std::cout << "\nYour grade is C+";
+    }
+    else if (mark < 75)
+    {
+        std::cout << "\nYour grade is B";
+    }
+    else if (mark < 80)
+    {
+        std::cout << "\nYour grade is B+";
+    }
+    else if (mark < 90)
+    {
+        std::cout << "\nYour grade is A";
+    }
+    else if (mark <= 100)
+    {
+        std::cout << "\nYour grade is A+";
+    }
+    return 0;
+}
