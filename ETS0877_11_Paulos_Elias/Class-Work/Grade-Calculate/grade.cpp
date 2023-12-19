a5f741d851a0bb9ec66bda355d785b369a353223
paulusel <paulusel@proton.me>
2023-12-19 06:58:25 +0000

    Add class work 2

---
+++ ETS0877_11_Paulos_Elias/Class-Work/Grade-Calculate/grade.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int main(){
+    float mid = 0, final = 0, total = 0;
+
+    int trial = 3;
+    for(;trial > 0; trial--){
+        std::cout << "Enter mid score" << std::endl;
+        std::cin >> mid;
+
+        if(mid > 40 || mid < 0)
+            std::cout << "Wrong scrore! Mid has to be in 0-40!" << std::endl;
+        else
+            break;
+    }
+
+    if(trial == 0){
+        std::cout << "Trial limit exceeded! Exiting ..." <<std::endl;
+        return 1;
+    }
+
+    trial = 3;
+    for(;trial > 0; trial--){
+        std::cout << "Enter final score" << std::endl;
+        std::cin >> final;
+
+        if(mid > 60 || mid < 0){
+            std::cout << "Wrong scrore! Final has to be in 0-60!" << std::endl;
+        }else {
+            break;
+        }
+    }
+
+    if(trial == 0){
+        std::cout << "Trial limit exceeded! Exiting ..." <<std::endl;
+        return 1;
+    }
+
+    total = mid + final;
+
+    std::cout << "Total mark: " << total << std::endl;
+
+    return 0;
+}
