74353ec1757001c9510c9d3b0d5e5cc34910fa6f
paulusel <paulusel@proton.me>
2024-01-15 17:52:11 +0000

    Initiate final project work

---
+++ Project/project.cpp
@@ -0,0 +1,15 @@
+/*
+ *Inventory management system
+ *
+ * This code is part of project for semester end.
+ *
+ *Byte Bandits, 2024.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //project here
+}
