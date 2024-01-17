d71b745bfdfd312e11d7e5cfccd961f6f080cf03
paulusel <paulusel@proton.me>
2024-01-17 12:47:36 +0000

    Create main framework

--- Project/project.cpp
+++ Project/project.cpp
@@ -11,13 +11,21 @@
 using namespace std;
 
 int main() {
+    int inventory[30][5][4] {};
+    string salesppl[4] {};
+    string products[5] {};
+
     while (true) { int option; cin>>option;
         cout<<"What to do?"<<endl;
         switch (option) {
             case 1:
+                break;
             case 2:
+                break;
             case 3:
+                break;
             case 4:
+                break;
             case 5:
                 return 0;
         }
