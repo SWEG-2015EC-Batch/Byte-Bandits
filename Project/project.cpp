14a527658df2069c9f11ca506a3ceeb059b1b4f0
Robel Desalegn <robeldesalegn07@gmail.com>
2024-01-17 17:57:40 +0300

    First commit

--- Project/project.cpp
+++ Project/project.cpp
@@ -1,25 +1,76 @@
-/*
- *Inventory management system
- *
- * This code is part of project for semester end.
- *
- *Byte Bandits, 2024.
- */
-
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-int main() {
-    while (true) { int option; cin>>option;
-        cout<<"What to do?"<<endl;
-        switch (option) {
+int main()
+{
+    int inventory[4][5] = {}; // 4 warehouses, 5 products
+    string salesppl[4] = {"Paulos", "Yonatan", "Zerubabel", "Robel"};
+    string products[5] = {"Mobile", "Computer", "Disk", "Charger", "Cable"};
+    int day = 0;
+
+    while (true)
+    {
+        int option;
+        cout << "What to do?" << endl
+             << "1. Input Report" << endl
+             << "2. Generate Report" << endl
+             << "3. Search" << endl
+             << "4. Calculate Bonus" << endl
+             << "5. Exit" << endl;
+
+        cin >> option;
+
+        switch (option)
+        {
+        case 1:
+            break;
+
+        case 2:
+            break;
+
+        case 3:
+            cout << "Search by:" << endl;
+            cout << "1. Product" << endl;
+            cout << "2. Salesperson" << endl;
+
+            int searchOption;
+            cin >> searchOption;
+
+            switch (searchOption)
+            {
             case 1:
+            {
+                string productName;
+                cout << "Enter product name to search: ";
+                getline(cin, productName);
+                for (int i = 0; i < 5; ++i)
+                {
+
+                    if (productName == products[1])
+                    {
+                       cout << "They are the same";
+                    }
+                }
+
+                break;
+            }
             case 2:
-            case 3:
-            case 4:
-            case 5:
-                return 0;
+                break;
+            }
+
+        case 4:
+            break;
+
+        case 5:
+            return 0;
+
+        default:
+            cout << "Invalid option. Please choose a valid option." << endl;
+            break;
         }
     }
+
+    return 0;
 }
