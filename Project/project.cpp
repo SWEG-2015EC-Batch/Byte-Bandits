61027f69f6e9118fd3f164eba1c56373c9b4e077
paulusel <paulusel@proton.me>
2024-01-17 18:30:29 +0000

    Polish the tabble output
    Finally acceptable table output code

--- Project/project.cpp
+++ Project/project.cpp
@@ -9,16 +9,66 @@ int main() {
     string products[5] {"Mobile", "Computer", "Disk", "Charger", "Cable"};
     int day = 0;
 
-    while (true) { int option; cin>>option;
+    while (true) {
+        int option;
         cout<<"What to do?"<<endl
             <<"1. Input Report"<<endl
             <<"2. Generate Report"<<endl
             <<"3. Search"<<endl
             <<"4. Calculate Bonus"<<endl
-            <<"4. Exit"<<endl;
+            <<"5. Exit"<<endl;
+        cin>>option;
         switch (option) {
            case 1:
-                {
+               break;
+           case 2:
+                 {
+                    constexpr int drow = 5, dcol = 4;
+                    int report[drow][dcol] {};
+                    for (int i = 0; i<30; ++i)
+                        for (int j = 0; j<5; ++j)
+                            for (int k = 0; k<4; ++k)
+                                report[j][k] += inventory[i][j][j];
+                    // print table
+                    int rows = 5, colmuns = 6;
+                    int width = 10, length = colmuns*(width+1);
+                    cout<<left;
+                    //output first line
+                    cout<<"+"; for (int k = 0; k<colmuns; ++k) { for (int j = 0; j<width; ++j) cout<<"-"; cout<<"+"; } cout<<endl;
+                    // column markers
+                    cout<<"|"<<setw(width)<<" "<<"|";
+                    for (int i = 0; i<dcol; ++i) cout<<setw(width)<<salesppl[i]<<"|";
+                    cout<<setw(width)<<"Total"<<"|"<<endl; //end of column markers
+                    //
+                    cout<<"+"; for (int k = 0; k<colmuns; ++k) { for (int j = 0; j<width; ++j) cout<<"-"; cout<<"+"; } cout<<endl;
+                    //
+                    int ctots[dcol] {}, ttot {};
+                    for (int i = 0; i<drow; ++i) {
+                        //row marker
+                        cout<<"|"<<setw(width)<<products[i]<<"|"; //end of row marker
+                        //actual data elements
+                        int rtot = 0;
+                        for (int j = 0; j<dcol; ++j) { 
+                            cout<<setw(width)<<report[i][j]<<"|";
+                            rtot += report[i][j];
+                            ctots[j] = report[i][j];
+                            ttot += rtot;
+                        }
+                        cout<<setw(width)<<rtot<<"|";
+                        cout<<endl;//end of data elements
+                        //line afterwards
+                        cout<<"+"; for (int k = 0; k<colmuns; ++k) { for (int j = 0; j<width; ++j) cout<<"-"; cout<<"+"; } cout<<endl;
+                    }
+                    //bottom total
+                    cout<<"|"<<setw(width)<<"Total"<<"|";
+                    for (int i = 0; i<dcol; ++i) cout<<setw(width)<<ctots[i]<<"|"; 
+                    /*total of total*/ cout<<setw(width)<<ttot<<"|"; cout<<endl;
+                    // line at the end
+                    cout<<"+"; for (int k = 0; k<colmuns; ++k) { for (int j = 0; j<width; ++j) cout<<"-"; cout<<"+"; } cout<<endl;
+                    break;
+                }
+            case 3:
+                 {
                     string productName;
                     cout << "Enter product name to search: ";
                     getline(cin, productName);
@@ -33,10 +83,6 @@ int main() {
 
                     break;
                 }
-            case 2:
-               break;
-            case 3:
-                break;
             case 4:
                 break;
 
@@ -46,8 +92,6 @@ int main() {
             default:
                 cout << "Invalid option. Please choose a valid option." << endl;
                 break;
-            }
         }
-
-    return 0;
+    }
 }
