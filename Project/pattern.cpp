7b08416681e37528f9f4cf9f617c3b69f659e743
paulusel <paulusel@proton.me>
2024-01-17 16:04:56 +0000

    Improve table pattern

--- Project/pattern.cpp
+++ Project/pattern.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
+ #include <iostream>
 #include <iomanip>
 
 using namespace std;
 
 int main() {
     cout<<left;
-    for (int k = 0; k<36; ++k) cout<<"_";
+    for (int k = 0; k<36; ++k) if (k%7==0) cout<<"+"; else cout<<"-";
     cout<<endl;
     for (int i = 0; i<5; ++i) {
         cout<<"|";
         for (int j = 0; j<5; ++j) { cout<<setw(6)<<"Paulo"<<"|";} 
         cout<<endl;
-        for (int k = 0; k<36; ++k) if (k%7==0) cout<<"|"; else cout<<"_";
+        for (int k = 0; k<36; ++k) if (k%7==0) cout<<"+"; else cout<<"-";
         cout<<endl;
     }
     return 0;
