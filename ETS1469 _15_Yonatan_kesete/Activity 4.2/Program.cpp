0fabde1496eaea05d77e5b5e4f0657f3e31100a9
Yonatanabu <149326410+Yonatanabu@users.noreply.github.com>
2024-01-24 23:33:12 +0300

    Create Program.cpp

---
+++ ETS1469 _15_Yonatan_kesete/Activity 4.2/Program.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+    const int MAX_LEN = 50;
+    char tvPrograms[2][MAX_LEN] = {{"Game of Thrones"}};
+    strcpy(tvPrograms[1], "The Big Bang Theory");
+
+    std::cout << "My favorite TV programes are " << tvPrograms[0] << " and " << tvPrograms[1] << std::endl;
+
+    return 0;
+}
