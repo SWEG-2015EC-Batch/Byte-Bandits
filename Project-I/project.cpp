b70b5c39cc3f96b2bd80409d22d542971ac2a490
paulusel <paulusel@proton.me>
2024-01-27 10:28:53 +0000

    Drop bonus option and some output update

--- Project-I/project.cpp
+++ Project-I/project.cpp
@@ -42,12 +42,11 @@ int main()
     {
         int option;
         cout << "\nWhat to do?" << endl
-             << "1. Input Report" << endl
+             << "1. Input Report" <<" (Day "<<(day%30)+1<<")"<< endl
              << "2. Generate Report" << endl
              << "3. Search" << endl
-             << "4. Calculate Bonus" << endl
-             << "5. Display Information" << endl // New option to display information
-             << "6. Exit" << endl;
+             << "4. Display Information" << endl // New option to display information
+             << "5. Exit" << endl;
         cin >> option;
         switch (option)
         {
@@ -107,7 +106,7 @@ int main()
                 while(qty < 0) {
                     cout << "\n\033[1;31mInvalid quantity!\033[0m\n" <<  "please enter a positive number: ";
                     cin >> qty;
-                    }
+                }
 
                 inventory[day][prindx][whouse - 1] = qty;
                 ++count;
@@ -129,10 +128,12 @@ int main()
                 for (int j = 0; j < nprod; ++j)
                     for (int k = 0; k < nwhouse; ++k)
                         report[j][k] += inventory[i][j][k];
-            
-            cout << "========================\n"
-         << right << setw(15) << "Report"
-         << "\n========================\n\n";
+
+            cout << setw(20)<<" "<<"========================\n"
+                << setw(20)<<" "<<right << setw(15) << "Report"<<"\n"
+                << setw(20)<<" "<<"========================\n\n";
+
+            cout<<"Total Days: "<<day<<"\n\n";
             // print table
             int colmuns = 6, width = 10;
             cout << left;
@@ -159,7 +160,7 @@ int main()
             }
             cout << endl;
             //
-            int ctots[dcol] = {}, ttot = {0};
+            int ctot[dcol] = {}, ttot = {0};
             for (int i = 0; i < drow; ++i) {
                 // row marker
                 cout << vln << setw(width) << products[i] << vln; // end of row marker
@@ -169,7 +170,7 @@ int main()
                 {
                     cout << setw(width) << report[i][j] << vln;
                     rtot += report[i][j];
-                    ctots[j] += report[i][j];
+                    ctot[j] += report[i][j];
                 } // end of data elements
                 // total at the end
                 ttot += rtot;
@@ -186,8 +187,21 @@ int main()
             // bottom total
             cout << vln << setw(width) << "Total" << vln;
             for (int i = 0; i < dcol; ++i)
-                cout << setw(width) << ctots[i] << vln;
+                cout << setw(width) << ctot[i] << vln;
             cout << setw(width) << ttot << vln << endl; /*total of total*/
+            // line at the end
+            cout << jnt;
+            for (int k = 0; k < colmuns; ++k) {
+                for (int j = 0; j < width; ++j)
+                    cout << hln;
+                cout << jnt;
+            }
+             // display bonuses
+            cout << endl;
+            cout << vln << setw(width) << "Bonus" << vln;
+            for (int i = 0; i < dcol; ++i)
+                cout << setw(width) << ctot[i] * bon_rate << vln;
+            cout << setw(width) << ttot * bon_rate << vln << endl; /*total bonus */
             // line at the end
             cout << jnt;
             for (int k = 0; k < colmuns; ++k) {
@@ -200,21 +214,16 @@ int main()
         }
         case 3:
         {
+        SearchChoice:
             cout << "Search by:" << endl;
             cout << "1. Product" << endl;
             cout << "2. Salesperson" << endl;
-            cout << "3. back to Menu" << endl;
+            cout << "3. Back to Menu" << endl;
 
             int searchOption;
             cin >> searchOption;
-            switch (searchOption)
-            {
-            case 1:
-            {
-                string productName;
-                cout << "Enter product name to search: ";
-                cin.clear();
-                cin >> productName;
+            if (searchOption == 3) break; // Exit by choice
+            switch (searchOption) { case 1: { string productName; cout << "Enter product name to search: "; cin.clear(); cin >> productName;
                 int prodIndex = -1;
                 for (int i = 0; i < nprod; ++i) {
                     if (!strncasecmp(products[i].c_str(), productName.c_str(), min(products[i].size(), productName.size())))
@@ -224,8 +233,8 @@ int main()
                     }
                 }
                 if (prodIndex == -1) {
-                    cout << "\n\033[1;31mNo product found!\033[0m\n";
-                    break;
+                    cout << "\n\033[1;31mNo product found!\033[0m\n\n";
+                    goto SearchChoice;
                 }
 
                 const int drow = nday, dcol = nwhouse;
@@ -299,8 +308,8 @@ int main()
                 }
                 if (prodIndex == -1)
                 {
-                    cout << "\n\033[1;31mNo salesperson found!\033[0m\n";
-                    break;
+                    cout << "\n\033[1;31mNo salesperson found!\033[0m\n\n";
+                    goto SearchChoice;
                 }
 
                 const int drow = nday, dcol = nprod;
@@ -343,8 +352,7 @@ int main()
                         rtot += inventory[i][j][prodIndex];
                     }
                     ttot += rtot;
-                    cout << setw(width) << rtot << vln << endl;
-                    // end of data elements
+                    cout << setw(width) << rtot << vln << endl; // end of data elements
                     // line afterwards
                     cout << jnt;
                     for (int k = 0; k < colmuns; ++k)
@@ -357,71 +365,14 @@ int main()
                 }
                 break;
             }
-            case 3:
-            {
-                break;
-            }
             default:
-                std::cout << "Invalid option!";
-                break;
+                cout << "\n\033[1;31mInvalid option!\033[0m\n\n";
+                goto SearchChoice;
             }
             break;
         }
-        case 4:
-        {
-            int total[nwhouse] = {};
-            float bonus[nwhouse] = {};
-            for (int i = 0; i < nday; ++i)
-                for (int j = 0; j < nprod; ++j)
-                    for (int k = 0; k < nwhouse; ++k)
-                        total[k] += inventory[i][j][k];
-
-            for (int i = 0; i < nwhouse; ++i)
-                bonus[i] = total[i] * bon_rate;
-
-            // make table
-            int rows = nwhouse, colmuns = 3, width = 10;
-            cout << left;
-            // output first line
-            cout << jnt;
-            for (int k = 0; k < colmuns; ++k)
-            {
-                for (int j = 0; j < width; ++j)
-                    cout << hln;
-                cout << jnt;
-            }
-            cout << endl;
-            // column markers
-            cout << vln << setw(width) << "" << vln << setw(width) << "Total" << vln << setw(width) << "Bonus" << vln << endl; // end of column markers
-            cout << jnt;
-            for (int k = 0; k < colmuns; ++k)
-            {
-                for (int j = 0; j < width; ++j)
-                    cout << hln;
-                cout << jnt;
-            }
-            cout << endl;
-            for (int i = 0; i < rows; ++i)
-            {
-                // row marker
-                cout << vln << setw(width) << salesppl[i] << vln; // end of row marker
-                // actual data elements
-                cout << setw(width) << total[i] << vln << setw(width) << bonus[i] << vln << endl; // end of data elements
-                // line afterwards
-                cout << jnt;
-                for (int k = 0; k < colmuns; ++k)
-                {
-                    for (int j = 0; j < width; ++j)
-                        cout << hln;
-                    cout << jnt;
-                }
-                cout << endl;
-            }
-            break;
-        }
-        case 5:
+       case 4:
         { // Option to display information
-        
             cout << "\nCurrent Information:\n";
             cout << "---------------------\n";
             cout << "Day: " << day + 1 << "\n";
@@ -434,11 +385,12 @@ int main()
             cout << "\n---------------------\n";
             break;
         }
-        case 6:
+        case 5:
             return 0;
 
         default:
-            cout << "Invalid option. Please choose a valid option." << endl;
+            cout << "" << endl;
+            cout << "\n\033[1;31mInvalid option. Please choose a valid option!\033[0m\n";
             break;
         }
     }
